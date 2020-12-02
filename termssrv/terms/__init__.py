0021aedda133e47becf30ab1cf8e1a05389fe053
Rim Valiulin <rim.valiulin@gmail.com>
2020-12-02 15:21:13 +0500

    fix app verbose name in admin

---
+++ termssrv/terms/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'termssrv.terms.apps.TermsConfig'
--- termssrv/terms/apps.py
+++ termssrv/terms/apps.py
@@ -3,5 +3,5 @@ from django.utils.translation import gettext_lazy as _
 
 
 class TermsConfig(AppConfig):
-    name = 'terms'
-    verbose_name = _('Terminology system')
+    name = 'termssrv.terms'
+    verbose_name = _('Terminology service')
