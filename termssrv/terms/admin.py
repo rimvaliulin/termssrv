fbb60956fc657908268f0ce64fd0a0bc4c62564d
Rim Valiulin <rim.valiulin@gmail.com>
2020-12-02 15:31:26 +0500

    fix typo

--- termssrv/terms/admin.py
+++ termssrv/terms/admin.py
@@ -4,9 +4,9 @@ from .models import Book, Term
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"short_name": ("name",)}
-    list_display = ('name', 'short_name', 'version', 'date_added')
-    ordering = ('-date_added',)
+    prepopulated_fields = {'short_name': ('name',)}
+    list_display = ('name', 'short_name', 'version', 'pub_date')
+    ordering = ('-pub_date',)
 
 
 @admin.register(Term)
