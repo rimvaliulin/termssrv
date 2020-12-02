1ac99e8e7a5529ea812a41783fb89cfa0767942d
Rim Valiulin <rim.valiulin@gmail.com>
2020-12-02 13:57:43 +0500

    add a reference book model

--- termssrv/terms/models.py
+++ termssrv/terms/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Book(models.Model):
+    """
+    The refefence book of terms.
+
+    Version must be unique within the reference book.
+
+    Name, short name and publication date are required.
+    Description is optional.
+    """
+
+    name = models.CharField(_('name'), max_length=150)
+    short_name = models.CharField(_('short name'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    version = models.CharField(_('version'), max_length=30)
+    pub_date = models.DateField(_('date'), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _('Reference Book')
+        verbose_name_plural = _('Reference Books')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['short_name', 'name', 'version'],
+                name='unique_book_version',
+            ),
+            models.UniqueConstraint(
+                fields=['name', 'version'],
+                name='unique_book_name_per_version',
+            ),
+            models.UniqueConstraint(
+                fields=['short_name', 'version'],
+                name='unique_book_short_name_per_version',
+            ),
+        ]
+
+    def __str__(self):
+        return f'{self.short_name} ({self.version})'
