d0aa38df275a1aa76b7c3bcbe06eaf7304200b0f
Rim Valiulin <rim.valiulin@gmail.com>
2020-12-03 10:48:42 +0500

    fix recent books method and add another staff

--- termssrv/terms/views.py
+++ termssrv/terms/views.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from rest_framework import serializers, status, viewsets
+from rest_framework import serializers, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Book, Term
@@ -8,40 +8,56 @@ from .serializers import BookSerializer, TermSerializer
 
 class BookViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows books to be viewed
+    API endpoint that allows books and terms to be viewed and validated.
     """
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    @action(
-        detail=False,
-        url_path=r'(?P<date>[0-9]{4}-[0-9]{2}-[0-9]{2})'
-    )
+    @action(detail=False, url_path=r'(?P<date>[0-9]{4}-[0-9]{2}-[0-9]{2})')
     def recent_books(self, request, date=None):
-        """получение списка справочников, актуальных на указанную дату"""
-        queryset = self.get_queryset().order_by('-pub_date')
+        """Get a list of reference books relevant on a given date"""
+        queryset = self.get_queryset()
         try:
             queryset = self.queryset.filter(pub_date__gte=date)
         except ValidationError as e:
             raise serializers.ValidationError(detail=e.messages)
-        else:
-            page = self.paginate_queryset(queryset)
-            serializer = self.get_serializer(page or queryset, many=True)
-            return Response(serializer.data)
+        queryset = queryset.order_by('-pub_date')
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
-    @action(detail=True, url_path=r'terms')
-    def current_terms(self, request, pk=None):
+    def retrieve(self, request, pk=None):
+        """Get the elements of a given directory of the current version"""
+        recent = self.get_queryset()
+        recent = recent.filter(short_name=pk).order_by('-pub_date')[:1]
+        queryset = Term.objects.filter(book=recent).order_by('code')
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = TermSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = TermSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
         return Response([])
 
-    @action(detail=True, url_path=r'terms', methods=['post'])
-    def validate_current_terms(self, request, pk=None):
-        return Response([])
+    @action(detail=True, url_path=r'(?P<version>[\w_]+)')
+    def terms_by_version(self, request, pk=None, version=None):
+        """Get the elements of a given reference book of the specified version"""
+        queryset = Term.objects.filter(
+            book__short_name=pk, book__version=version
+        ).order_by('code')
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = TermSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = TermSerializer(queryset, many=True)
+        return Response(serializer.data)
 
-    @action(detail=True, url_path=r'terms/<str:version>/')
-    def terms_by_version(self, request, pk=None):
-        return Response([])
-
-    @action(detail=True, url_path=r'terms/<str:version>/', methods=['post'])
+    @action(detail=True, url_path=r'(?P<version>[\w_]+)', methods=['post'])
     def validate_term_by_version(self, request, pk=None):
         return Response([])
